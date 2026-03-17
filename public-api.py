ccd041681b2f540ed0ea3a9ee0d6c71518cbdb23
Mohammad Shafiei <m.shafiei6674@gmail.com>
2026-03-17 17:00:36 +0100

    Add files via upload

---
+++ public-api.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+from random import randint, choice
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    phrases: list[str] = ['Welcome to this page!', 'You are looking good today!']
+    return {'phrase': choice(phrases),'date' : datetime.now()}
+
+@app.route('/api/random')
+def random():
+    number_input = request.args.get('number', type=int)
+    text_input = request.args.get('text', type=str)
+    if isinstance(number_input, int):
+        return { 'input': number_input,
+                 'random': randint(0, number_input),
+                 'text': text_input,
+                 'date': datetime.now()}
+    else:
+        return {'Error': 'Please only enter numbers.'}
+
+if __name__ == '__main__':
+    app.run()
